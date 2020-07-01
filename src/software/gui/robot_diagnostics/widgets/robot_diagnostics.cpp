9a30d676c61c1bca692745a59a376bf7796af1db
licody <licody2000@gmail.com>
2020-06-30 19:17:15 -0700

    remove comments

--- src/software/gui/robot_diagnostics/widgets/robot_diagnostics.cpp
+++ src/software/gui/robot_diagnostics/widgets/robot_diagnostics.cpp
@@ -75,10 +75,10 @@ void RobotDiagnostics::updateRobotDiagnostics()
     {
         updateFeedback(main_widget, sensor_msg.value());
         // update robot status table
-        /*for (const auto& robot_msg : sensor_msg.value().tbots_robot_msgs())
+        for (const auto& robot_msg : sensor_msg.value().tbots_robot_msgs())
         {
             main_widget->robot_status_table_widget->updateTbotsRobotMsg(robot_msg);
-        }*/
+        }
         sensor_msg = sensor_msg_buffer->popLeastRecentlyAddedValue();
     }
 }
